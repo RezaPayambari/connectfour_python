3a431de53540a690a243e93e8afd0080216a7590
Fabian Golle <me@fabian-golle.de>
2012-12-27 19:40:29 +0100

    First working copy without any winning-rules & comfort

--- connectfour.py
+++ connectfour.py
@@ -12,6 +12,8 @@ class Connectfour(object):
 
 	identifier = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
+	whosTurn = 1;
+
 	def setup(self):
 		self.field = [[0 for x in range(self.fieldsize[1])] for x in range(self.fieldsize[0])]
 
@@ -22,6 +24,8 @@ class Connectfour(object):
 			for p in itertools.permutations(self.identifier, 2):
 			    ident.append(''.join(p))
 			self.identifier = self.identifier + ident
+
+		self.identifier = self.identifier[0:self.fieldsize[1]+1]
 
 	def getTranslation(self, content):
 		if content == 0:
@@ -39,7 +43,7 @@ class Connectfour(object):
 
 	def printField(self):
 		self._printNewLine()
-		for row in self.field:
+		for row in reversed(self.field):
 			print('| ', end='')
 			for column in row:
 				print(self.getTranslation(column) + ' | ', end='')
@@ -52,17 +56,31 @@ class Connectfour(object):
 		print(line)
 
 
-	def feed(self, where, what):
-		self.field[where[0]][where[1]] = what
+	def feed(self, where):
+		if (where not in self.identifier):
+			return False
+
+		# Find out if slot can be used
+		columnNumber = self.identifier.index(where)
+		rowNumber = 0
+		i = 0
+		for row in self.field:
+			if (row[columnNumber] == 0):
+				rowNumber = i
+				break
+			i += 1
+
+
+		# Save selection to field
+		self.field[rowNumber][columnNumber] = self.whosTurn
+
+		# Next player is on turn
+		self.whosTurn = 1 if self.whosTurn == 2 else 2
 
 c4 = Connectfour()
-
 c4.setup();
 
-c4.feed([5,6], 1)
-c4.feed([5,5], 1)
-c4.feed([5,4], 2)
-c4.feed([4,6], 2)
-c4.feed([4,4], 1)
+for x in range(1,10):
+	c4.feed(input(">> "))
+	c4.printField()
 
-c4.printField()
