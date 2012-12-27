bd905c360942922888f9d009f86b4d30b5189220
Fabian Golle <me@fabian-golle.de>
2012-12-27 22:23:40 +0100

    Working RC

--- connectfour.py
+++ connectfour.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+from random import choice
+from os import system
+from time import sleep
+import re
+
 class Connectfour(object):
 	"""Connectfour Game on CLI Basis"""
 	def __init__(self):
@@ -10,7 +15,7 @@ class Connectfour(object):
 
 	field = []
 
-	identifier = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+	identifier = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','V','W','X','Y','Z']
 
 	whosTurn = 1;
 
@@ -29,7 +34,7 @@ class Connectfour(object):
 			    ident.append(''.join(p))
 			self.identifier = self.identifier + ident
 
-		self.identifier = self.identifier[0:self.columnCount+1]
+		self.identifier = self.identifier[0:self.columnCount]
 
 
 	def getTranslation(self, content):
@@ -47,6 +52,7 @@ class Connectfour(object):
 		print(line)
 
 	def printField(self):
+		print("\nVIER GEWINNT\n")
 		self._printNewLine()
 		for row in reversed(self.field):
 			print('| ', end='')
@@ -77,7 +83,6 @@ class Connectfour(object):
 		# Check vertically
 		for row in range(self.rowCount-3):
 			for col in range(self.columnCount):
-				print("Checking col "+str(col)+" row "+str(row));
 				if (self.field[row][col] == self.field[row+1][col] == self.field[row+2][col] == self.field[row+3][col] != 0):
 					return self.field[row][col]
 
@@ -99,9 +104,17 @@ class Connectfour(object):
 
 		return False
 
+	def error(self, message):
+		if (self.whosTurn == 2 and self.aienemy == True):
+			return True
+
+		print("FEHLER: "+str(message));
+		sleep(1)
 
 	def feed(self, where):
+		where = where.upper()
 		if (where not in self.identifier):
+			self.error("Ungültiger Slot!")
 			return False
 
 		# Find out if slot can be used
@@ -113,7 +126,7 @@ class Connectfour(object):
 			rowNumber += 1
 
 		if (rowNumber >= self.rowCount):
-			print("Diese Reihe ist Voll!")
+			self.error("Diese Reihe ist Voll - Wählen Sie bitte eine Andere!")
 			return False
 
 
@@ -123,15 +136,116 @@ class Connectfour(object):
 		# Next player is on turn
 		self.whosTurn = 1 if self.whosTurn == 2 else 2
 
-c4 = Connectfour()
-c4.setup();
+		if (self.whosTurn == 2 and self.aienemy == True):
+			# Make AI-Move
+			print("Computergegner ist an der Reihe..");
+			self._aiMakeMove()
 
-while (c4.isNotFull()):
-	c4.feed(input("Spieler "+str(c4.whosTurn)+" >> "))
-	win = c4.checkWin()
-	c4.printField()
-	if (win):
-		print ("Spieler "+str(win)+" hat gewonnen!")
-		quit();
+		return True
 
-print("Spielende.")
+
+	def _aiMakeMove(self):
+		while True:
+			selection = choice(self.identifier)
+			if (self.feed(selection)):
+				print("Computergegner wählt "+selection);
+				sleep(1)
+				break
+
+def main():
+	system("clear")
+	Game = Connectfour()
+
+	print ("""
+ __      ___                                 _             _   
+ \ \    / (_)                               (_)           | |  
+  \ \  / / _  ___ _ __    __ _  _____      ___ _ __  _ __ | |_ 
+   \ \/ / | |/ _ \ '__|  / _` |/ _ \ \ /\ / / | '_ \| '_ \| __|
+    \  /  | |  __/ |    | (_| |  __/\ V  V /| | | | | | | | |_ 
+     \/   |_|\___|_|     \__, |\___| \_/\_/ |_|_| |_|_| |_|\__|
+                          __/ |                                
+                         |___/                                 
+	""")
+
+	print("Copyright 2012 Fabian Golle, Veronika Schoepf. Alle Rechte vorbehalten\n\n");
+	print("Sie können das Spiel jederzeit mit der Eingabe von Q beenden!")
+	print("Herzlich Willkommen!\nWählen Sie einen Spielmodus!")
+	while True:
+		print("Computergegner (C) oder menschlicher Gegner (M)?");
+		modus = input(">> ")
+		if (modus.upper() == "Q"):
+			print("Bye-Bye ;)");
+			quit();
+		elif (modus.upper() == "C"):
+			Game.aienemy = True
+			print("\nSie haben: X")
+			print("Computergegner hat: O")
+			break
+		elif (modus.upper() == "M"):
+			Game.aienemy = False
+			print("\nSpieler 1 hat: X")
+			print("Spieler 2 hat: O")
+			break
+
+	while True:
+		print("\nWie groß soll das Spielfeld sein? Breite x Höhe (Leere Eingabe = Standardgröße 6x7)");
+		sizeI = input(">> ")
+		size = re.search("(\d)+( )?x( )?(\d)+", sizeI)
+		if (sizeI == ""):
+			break
+		elif (size):
+			Game.fieldsize = [int(size.group(1)), int(size.group(4))]
+			break
+
+	system("clear")
+
+	Game.setup();
+	Game.printField()
+
+	while (Game.isNotFull()):
+		whateverTheUserEntered = input("Spieler "+str(Game.whosTurn)+" >> ")
+		if (whateverTheUserEntered.upper() == "Q"):
+			print("Bye-Bye ;)");
+			quit();
+		Game.feed(whateverTheUserEntered)
+		system("clear")
+		win = Game.checkWin()
+		Game.printField()
+		if (win):
+			if (win == 1 or (win == 2 and self.aienemy == False)):
+				for x in range(4):
+					system("clear")
+					print("""
+  /$$$$$$                       /$$               /$$             /$$     /$$                     /$$
+ /$$__  $$                     | $$              | $$            | $$    |__/                    | $$
+| $$  \__/  /$$$$$$  /$$$$$$  /$$$$$$   /$$   /$$| $$  /$$$$$$  /$$$$$$   /$$  /$$$$$$  /$$$$$$$ | $$
+| $$ /$$$$ /$$__  $$|____  $$|_  $$_/  | $$  | $$| $$ |____  $$|_  $$_/  | $$ /$$__  $$| $$__  $$| $$
+| $$|_  $$| $$  \__/ /$$$$$$$  | $$    | $$  | $$| $$  /$$$$$$$  | $$    | $$| $$  \ $$| $$  \ $$|__/
+| $$  \ $$| $$      /$$__  $$  | $$ /$$| $$  | $$| $$ /$$__  $$  | $$ /$$| $$| $$  | $$| $$  | $$    
+|  $$$$$$/| $$     |  $$$$$$$  |  $$$$/|  $$$$$$/| $$|  $$$$$$$  |  $$$$/| $$|  $$$$$$/| $$  | $$ /$$
+ \______/ |__/      \_______/   \___/   \______/ |__/ \_______/   \___/  |__/ \______/ |__/  |__/|__/
+                                                                                                     
+""")
+					print ("Spieler "+str(win)+" hat gewonnen!")
+					sleep(1)
+					system("clear")
+					print("""
+ $$$$$$\                      $$\               $$\            $$\     $$\                     $$\ 
+$$  __$$\                     $$ |              $$ |           $$ |    \__|                    $$ |
+$$ /  \__| $$$$$$\  $$$$$$\ $$$$$$\   $$\   $$\ $$ | $$$$$$\ $$$$$$\   $$\  $$$$$$\  $$$$$$$\  $$ |
+$$ |$$$$\ $$  __$$\ \____$$\\_$$  _|  $$ |  $$ |$$ | \____$$\\_$$  _|  $$ |$$  __$$\ $$  __$$\ $$ |
+$$ |\_$$ |$$ |  \__|$$$$$$$ | $$ |    $$ |  $$ |$$ | $$$$$$$ | $$ |    $$ |$$ /  $$ |$$ |  $$ |\__|
+$$ |  $$ |$$ |     $$  __$$ | $$ |$$\ $$ |  $$ |$$ |$$  __$$ | $$ |$$\ $$ |$$ |  $$ |$$ |  $$ |    
+\$$$$$$  |$$ |     \$$$$$$$ | \$$$$  |\$$$$$$  |$$ |\$$$$$$$ | \$$$$  |$$ |\$$$$$$  |$$ |  $$ |$$\ 
+ \______/ \__|      \_______|  \____/  \______/ \__| \_______|  \____/ \__| \______/ \__|  \__|\__|
+                                                                                                                                                                                               
+""")
+					print ("Spieler "+str(win)+" hat gewonnen!")
+					sleep(1)
+			break
+
+	print("Spielende.")
+
+
+if __name__ == "__main__":
+    main()
